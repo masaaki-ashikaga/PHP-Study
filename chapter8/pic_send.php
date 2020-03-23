89bba3a77e92432d8e148a5505d32d3f74fcdad3
masaaki-ashikaga <gamasaga0312@gmail.com>
2020-03-23 21:27:02 +0900

    chapter8まで完了

---
+++ chapter8/pic_send.php
@@ -0,0 +1,12 @@
+<html>
+  <head>
+    <meta charset="utf-8">
+  </head>
+  <body>
+    <h1>画像をアップ</h1>
+    <form action="pic_receive.php" method="post" enctype="multipart/form-data">
+      <p><input type="file" name="img"></p>
+      <p><input type="submit" value="送信"></p>
+    </form>
+  </body>
+</html>
